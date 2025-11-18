ad7f177bec77791be06d526cbfdbec32d353bcbb
Justin McKague <justin.mckague1@gmail.com>
2025-11-17 21:11:20 -0400

    Basic shave-down mechanic function
    player character gets shaven down while sliding on ground

--- grind-set/PackedScenes/player.gd
+++ grind-set/PackedScenes/player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-const speed = 550
+const speed:float = 550
 const jump_power = -2400
 
 const accel = 75
@@ -10,19 +10,30 @@ const gravity = 125
 
 @onready var sprite = $Mask
 
+@export var right_particles: GPUParticles2D
+@export var left_particles: GPUParticles2D
+
 var initial_scale: Vector2
 
-func _ready() -> void:
-	initial_scale = scale
+var on_slab = false
+
+signal grind(direction, decay)
 
 func _physics_process(delta: float) -> void:
-	var input_dir: Vector2 = read_input()
+	var input_dir: Vector2 = movement()
 	
 	if input_dir != Vector2.ZERO:
 		acceleration(input_dir)
-
+		if is_on_floor():
+			grind.emit(Vector2.DOWN, 0.001)
+			if input_dir.x > 0:
+				left_particles.emitting = true
+				right_particles.emitting = false
+			else:
+				left_particles.emitting = false
+				right_particles.emitting = true
 		var tween = sprite.create_tween()
-		tween.tween_property(sprite, "skew", -0.25 * input_dir.x, 0.15)
+		tween.tween_property(sprite, "skew", -0.15 * input_dir.x, 0.15)
 		
 	else:
 		add_friction()
@@ -30,8 +41,14 @@ func _physics_process(delta: float) -> void:
 		tween.tween_property(sprite, "skew", 0, 0.07)
 	move_and_slide()
 	jump()
+	if on_slab == true and Input.is_action_just_pressed("combine"):
+		# combine logic here
+		print("combined with slab")
+	if velocity.x == 0:
+		right_particles.emitting = false
+		left_particles.emitting = false
 	
-func read_input() -> Vector2:
+func movement() -> Vector2:
 	var input_dir = Vector2.ZERO
 	
 	input_dir.x = Input.get_axis("left", "right")
@@ -46,10 +63,21 @@ func add_friction():
 
 func jump():
 	if Input.is_action_just_pressed("jump") and is_on_floor():
+		right_particles.emitting = false
+		left_particles.emitting = false
+		initial_scale = sprite.scale
 		velocity.y = jump_power
 		var tween = create_tween()
-		tween.tween_property(sprite, "scale", Vector2(1.5, 0.6), 0.025)
-		tween.tween_property(sprite, "scale", Vector2(0.6, 1.5), 0.15)
+		tween.tween_property(sprite, "scale", sprite.scale * Vector2(1.5, 0.6), 0.025)
+		tween.tween_property(sprite, "scale", sprite.scale * Vector2(0.6, 1.5), 0.15)
 		tween.tween_property(sprite, "scale", initial_scale, 0.1)
 	else:
 		velocity.y += gravity
+
+func _on_area_2d_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Slab"):
+		on_slab = true
+
+func _on_area_2d_area_exited(area: Area2D) -> void:
+	if area.is_in_group("Slab"):
+		on_slab = false
